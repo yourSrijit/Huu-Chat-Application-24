a8015f81ccfb561d631a4e31b81d0f66423fec31
Srijit Bera <berasrijit02@gmail.com>
2024-07-31 23:07:53 +0530

    Delete Real Time Chatting Application directory

--- Real Time Chatting Application/backend/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
-let router=express.Router();
-
-router.post("/signup" ,signup)
-router.post("/login",login)
-router.post("/logout",logout)
-
-export default router; 
